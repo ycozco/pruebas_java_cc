952fb7ef0583cdcfea0661e846b4b12590971fd3
YOSET C <64040965+ycozco@users.noreply.github.com>
2024-03-21 23:56:07 +0000

    upload reviewed files

--- Ejercicio5/HabitacionEstandar.java
+++ Ejercicio5/HabitacionEstandar.java
@@ -2,12 +2,12 @@ package Ejercicio5;
 
 public class HabitacionEstandar extends Habitacion {
     
-    public HabitacionEstandar (int numeroHabitacion, double precioPorNoche){
+    public HabitacionEstandar(int numeroHabitacion, double precioPorNoche) {
         super(numeroHabitacion, precioPorNoche);
     }
 
     @Override
-    public double calcularCosto (int diasReserva){
-        return diasReserva*this.getPrecioPorNoche();
+    public double calcularCosto(int diasReserva) {
+        return diasReserva * getPrecioPorNoche();
     }
 }
