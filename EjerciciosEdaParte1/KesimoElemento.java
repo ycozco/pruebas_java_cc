000d3143d0a8ccb7228094bc7297b5e49fc3d242
cmezav <120595451+cmezav@users.noreply.github.com>
2024-04-02 01:44:17 +0000

    kesimo elemento

---
+++ EjerciciosEdaParte1/KesimoElemento.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+public class KesimoElemento{
+    public static int encontrarK (int [] arreglo, int k) {
+        int tamano = arreglo.length;
+        int min = 0, max = tamano-1;
+        
+        while(min<=max) {
+            int pivot = arreglo[max];
+            int locPivot = min;
+
+            for(int i=min; i<=max; i++){
+                if(arreglo[i]<pivot){
+                    int temp = arreglo[i];
+                    arreglo[i] = arreglo[locPivot];
+                    arreglo[locPivot] = temp;
+                    locPivot++;                                                                    
+                }
+            }
+            int temp = arreglo[locPivot];
+            arreglo[locPivot] = arreglo[max];
+            arreglo[max] = temp;
+
+            if(locPivot==k-1) {
+                return arreglo[locPivot];
+            } else if (locPivot>k-1) {
+                max=locPivot-1; 
+            } else {
+                min=locPivot+1;
+            } 
+        }
+        return -1;
+    }
+
+    public static void main (String [] args) {
+        int [] arreglo2 = {4,6,8,9,1,2};
+        // 1,2,4,6,8,9
+        // generar arreglo de 100 elementos aleatorio
+        Random random = new Random();
+        int[] arreglo = new int[100];
+        String cadena = "";
+        for (int i = 0; i < arreglo.length; i++) {
+            int elemento = random.nextInt(100);
+            cadena += elemento + ", ";
+            arreglo[i] = elemento;
+            
+        }
+        
+        int k = 15;
+        System.out.println("Arreglo: " + cadena);
+        System.out.println("k esimo elemento: " + encontrarK(arreglo, k)); 
+    }
+
+}
--- EjerciciosEdaParte1/MedianaSinOrdenar.java
+++ EjerciciosEdaParte1/MedianaSinOrdenar.java
@@ -1,5 +1,18 @@
 package EjerciciosEdaParte1;
 
 public class MedianaSinOrdenar {
-    
+    public static void main(String [] args) {
+        int [] arreglo = {3,4,2,6,8,10,5,7,11,1}
+        System.out.println("La mediana es: "+ encontrarMediana(arreglo) );
+
+    }
+
+    public static double encontrarMediana(int [] arreglo){
+
+        return 0.0;  
+    }
+
+    public static int encontrarMenor(int [] arreglo, int menor, int mayor) {
+        
+    }
 }
