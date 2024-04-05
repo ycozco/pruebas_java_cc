f17cd8ed2ab1e32a681f1488d1bd2c8cf5a348bb
cmezav <120595451+cmezav@users.noreply.github.com>
2024-04-05 00:35:07 +0000

    recursividad

--- EjerciciosEdaParte1/MenorCambio.java
+++ EjerciciosEdaParte1/MenorCambio.java
@@ -2,34 +2,49 @@ package EjerciciosEdaParte1;
 
 public class MenorCambio {
 
-    private static int [] memoria;
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         int [] monedas = {500, 200, 100, 50};
         int gasto = 650;
         int pago = 2000;
         int vuelto = pago-gasto;
-        System.out.println("El minimo de monedas es: " + menorCambio(monedas, vuelto));
-
-    }
-
-    public static int menorCambio (int [] monedas, int vuelto) {
-        if(vuelto==0) return 0;
-        int contadorMonedas=0;
-
-        for(int i=0; i<=monedas.length; i++){
-            if(vuelto%monedas[i]>=0 ){
-                int division = vuelto / monedas[i];
-                memoria[i]=division;
-                vuelto =- division*monedas[i];
+        int [] listadoMonedas = menorCambio(monedas, vuelto);
+        for (int i=0; i<listadoMonedas.length; i++){
+            if(listadoMonedas[i]>0){
+                System.out.println("Moneda: " + monedas[i]/100.0 + ": " + listadoMonedas[i]);
             }
         }
-        for (int i : memoria) {
-            contadorMonedas=+i;
+        System.out.println("Moneda: " + menorCambioRecursivo(monedas, vuelto, monedas.length-1));
+
+    }
+
+    public static int[] menorCambio (int [] monedas, int vuelto) {
+       
+        int [] listadoMonedas = new int [monedas.length] ;
+
+        for(int i=0; i<monedas.length; i++){
+            if(monedas[i]<=vuelto){
+                int division = vuelto / monedas[i];
+                listadoMonedas[i]=division;
+                vuelto -= division*monedas[i];
+            }
         }
 
-        return contadorMonedas;
+        return listadoMonedas;
     }
 
 
+    public static int  menorCambioRecursivo (int [] monedas, int vuelto, int posicion){
+        if(vuelto==0) return 0;
+        if(vuelto<0) return Integer.MAX_VALUE;
+
+        int monedaIncluida = menorCambioRecursivo(monedas, vuelto-monedas[posicion], posicion-1);
+        if(monedaIncluida!=Integer.MAX_VALUE){
+            monedaIncluida+=1;
+        } 
+        int monedaExcluida = menorCambioRecursivo(monedas, vuelto, posicion-1);
+        
+        return Math.min(monedaIncluida, monedaExcluida);
+        
+    }
 
 }
